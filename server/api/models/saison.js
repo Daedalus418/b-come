fa0230b5ba60e6a34788b3b7ef946e9cfef00e27
stephsen <stephsen59@gmail.com>
2017-01-26 20:56:42 +0100

    model saison

---
+++ server/api/models/saison.js
@@ -0,0 +1,134 @@
+import mongoose from 'mongoose';
+
+const saisonSchema = new mongoose.Schema({
+    saison: {
+        formation: {
+            nomformation: String,
+            image: String,
+            description: String,
+            date: Date,
+            place: Number,
+            placedispo: Number,
+            lien: String,
+            contenu: {
+                ContenuUn: {
+                    lien: String,
+                    descriptioncontenu: String
+                },
+                ContenuDeux: {
+                    lien: String,
+                    descriptioncontenu: String
+                },
+                ContenuTrois: {
+                    lien: String,
+                    descriptioncontenu: String
+                }
+            },
+            commentaire: {
+                commentaireUn: {
+                    user: String,
+                    commentaire: String,
+                    like: Number
+                },
+                commentaireDeux: {
+                    user: String,
+                    commentaire: String,
+                    like: Number
+                },
+                commentaireTrois: {
+                    user: String,
+                    commentaire: String,
+                    like: Number
+                },
+                commentaireQuatre: {
+                    user: String,
+                    commentaire: String,
+                    like: Number
+                },
+                commentaireCinq: {
+                    user: String,
+                    commentaire: String,
+                    like: Number
+                },
+                commentaireSix: {
+                    user: String,
+                    commentaire: String,
+                    like: Number
+                }
+            },
+            quizz: {
+                userUn: {
+                    nom: String,
+                    result: Number
+                },
+                userDeux: {
+                    nom: String,
+                    result: Number
+                },
+                userTrois: {
+                    nom: String,
+                    result: Number
+                }
+            }
+        }
+
+    }
+});
+
+let model = mongoose.model('Saison', saisonSchema);
+
+export default class Saison {
+
+    findAll(req, res) {
+        model.find({}, (err, saisons) => {
+            if (err) {
+                res.sendStatus(403);
+            } else {
+                res.json(saisons);
+            }
+        });
+    }
+
+    findById(req, res) {
+        model.findById(req.params.id, (err, saison) => {
+            if (err || !saison) {
+                res.sendStatus(403);
+            } else {
+                res.json(saison);
+            }
+        });
+    }
+
+    create(req, res) {
+        model.create(req.body,
+            (err, saison) => {
+                if (err) {
+                    res.status(500).send(err.message);
+                } else {
+                    res.json(saison);
+                }
+            });
+    }
+
+    update(req, res) {
+        model.update({
+            _id: req.params.id
+        }, req.body, (err, saison) => {
+            if (err || !saison) {
+                res.status(500).send(err.message);
+            } else {
+                res.json(saison);
+            }
+        });
+    }
+
+    delete(req, res) {
+        model.findByIdAndRemove(req.params.id, (err) => {
+            if (err) {
+                res.status(500).send(err.message);
+            } else {
+                res.sendStatus(200);
+            }
+        });
+    }
+}
---
+++ server/api/routes/saisons.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import Saison from '../models/saison.js';
+
+let router = express.Router();
+
+module.exports = (app) => {
+
+    var saison = new Saison();
+
+    router.get('/formation', saison.findAll);
+
+    router.get('/formation:id', saison.findById);
+
+    router.post('/formation', saison.create);
+
+    router.put('/formation:id', saison.update);
+
+    router.delete('/formation:id', saison.delete);
+
+    app.use('/saisons', router);
+
+};
