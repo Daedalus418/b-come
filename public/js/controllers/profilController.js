bac0779fcc3f4cb372b8f1e7cd85ca757922bd81
Camjr <cfontainef1@gmail.com>
2017-01-27 02:32:23 +0100

    profil .user

--- public/js/controllers/profilController.js
+++ public/js/controllers/profilController.js
@@ -5,6 +5,7 @@ function profilController(userService, $routeParams) {
 
         this.load = () => {
             this.userService.getOne(this.userId).then((res) => {
+                console.log(res.data);
                 this.user = res.data;
             });
         };
